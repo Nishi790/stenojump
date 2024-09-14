8ddb0435c9f36749540caa9f364e3084c232f58e
Nishi790 <rebelnishi@gmail.com>
2024-09-14 12:44:59 -0400

    Story level map astar generation and basic movement begun
    Using placeholder assets. needs interaction interface

---
+++ story_mode/characters/socks_self_nav.gd
@@ -0,0 +1,86 @@
+class_name SelfNavCharacter
+extends Node2D
+
+@export var animations: AnimatedSprite2D
+
+var nav_astar: AStar2D
+
+var nav_path: PackedVector2Array
+var nav_index: int = 0
+var next_point: Vector2
+var destination: Vector2
+
+var direction: Vector2 = Vector2.ZERO
+var direction_changed: bool = false
+@export var speed: float = 300
+var navigating: bool
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		nav_to_interest_point(Vector2(5*16*6, 13*16*6))
+
+
+func _process(delta: float) -> void:
+	if direction_changed:
+		select_animation()
+
+
+
+func _physics_process(delta: float) -> void:
+
+	if nav_astar == null:
+		return
+
+	if navigating:
+		var old_dir: Vector2 = direction
+		direction = (next_point - position).normalized()
+		if direction != old_dir:
+			direction_changed = true
+		position = position.move_toward(next_point, speed * delta)
+		if position.is_equal_approx(next_point):
+			next_nav_point()
+
+
+func nav_to_interest_point(coords: Vector2) -> void:
+	print("Navigating to %s" % coords)
+	navigating = true
+	destination = coords
+	nav_path = nav_astar.get_point_path(nav_astar.get_closest_point(position), nav_astar.get_closest_point(destination))
+	next_nav_point()
+
+
+func next_nav_point() -> void:
+	if nav_path.size() == nav_index:
+		end_navigation()
+	else:
+		next_point = nav_path[nav_index]
+		nav_index += 1
+
+
+func end_navigation() -> void:
+	navigating = false
+	destination = Vector2.ZERO
+	next_point = Vector2.ZERO
+	nav_path.clear()
+
+
+func select_animation() -> void:
+	if navigating == false:
+		if direction.x < 0:
+			animations.flip_h = true
+		else:
+			animations.flip_h = false
+		animations.play("idle")
+	else:
+		if abs(direction.x) > abs(direction.y):
+			if direction.x < 0:
+				animations.flip_h = true
+			else:
+				animations.flip_h = false
+			animations.play("walk")
+		else:
+			if direction.y > 0:
+				animations.play("walk_down")
+			else:
+				animations.play("walk_up")
---
+++ story_mode/story_levels/level_1.gd
@@ -0,0 +1,64 @@
+class_name LessonLevelMap
+extends Node2D
+
+
+@export var waypoints: Array [Vector2]
+@export var interactables: Array[BaseInteractable]
+
+var waypoint_astar_grid: AStar2D
+var astar_nav_grid: AStar2D
+
+@export var tile_map_holder: Node2D
+@export var tile_map_base: TileMapLayer
+@export var tile_map_obstacles: TileMapLayer
+
+@export var player: SelfNavCharacter
+
+
+func _ready() -> void:
+	#Initialize AStar grid
+	astar_nav_grid = AStar2D.new()
+	var index: int = 0
+	var tile_size_x: Vector2i = Vector2i(tile_map_base.tile_set.tile_size.x * tile_map_holder.scale.x, 0)
+	var tile_size_y: Vector2i = Vector2i(0, tile_map_base.tile_set.tile_size.y * tile_map_holder.scale.y)
+	var top_point: Vector2i
+	var bottom_point: Vector2i
+	var left_point: Vector2i
+	var right_point: Vector2i
+
+	#Cycle through all floor tiles
+	for tile_coords in tile_map_base.get_used_cells():
+		var tile_data: TileData = tile_map_base.get_cell_tile_data(tile_coords)
+		if tile_data.get_custom_data("navigable"):
+
+			#Skip this tile if there is something non-navigable in the obstacle layer
+			var obstacle_check_data: TileData = tile_map_obstacles.get_cell_tile_data(tile_coords)
+			if obstacle_check_data != null and obstacle_check_data.get_custom_data("navigable") == false:
+				continue
+
+			#Add nav point and check for adjacent points (within 1 tile, all cardinal directions) and conncet them
+			var tile_world_coords: Vector2i = tile_map_base.map_to_local(tile_coords) * tile_map_holder.scale
+			astar_nav_grid.add_point(index, tile_world_coords)
+			top_point = tile_world_coords + tile_size_y
+			bottom_point = tile_world_coords - tile_size_y
+			left_point = tile_world_coords - tile_size_x
+			right_point = tile_world_coords + tile_size_x
+			var surrounding_points: Array[Vector2i] = [top_point, bottom_point, left_point, right_point]
+			for coord in surrounding_points:
+				var point_ID: int = astar_nav_grid.get_closest_point(coord)
+				if astar_nav_grid.get_point_position(point_ID) as Vector2i == coord:
+					astar_nav_grid.connect_points(point_ID, index)
+			index += 1
+
+	player.nav_astar = astar_nav_grid
+	#For testing: draw grid
+	queue_redraw()
+
+func _draw() -> void:
+	for point_id in astar_nav_grid.get_point_ids():
+		var point: Vector2i = astar_nav_grid.get_point_position(point_id)
+		draw_circle(point, 3, Color.GREEN)
+		var connected_points: PackedInt64Array = astar_nav_grid.get_point_connections(point_id)
+		for id in connected_points:
+			var connection_position: Vector2 = astar_nav_grid.get_point_position(id)
+			draw_line(point, connection_position, Color.WHITE)
