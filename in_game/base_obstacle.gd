f84c8bf4605b77da7e4d3ba271f768b154d2b434
Nishi790 <rebelnishi@gmail.com>
2024-09-24 18:28:11 -0400

    Fixed obstacles disappearing before fully off screen

--- in_game/base_obstacle.gd
+++ in_game/base_obstacle.gd
@@ -10,6 +10,7 @@ enum ObstacleType {JUMP, CRAWL}
 @export var type: ObstacleType = ObstacleType.JUMP
 @export var sprite: Sprite2D
 @export var collision_shape: CollisionShape2D
+@export var visible_on_screen: VisibleOnScreenNotifier2D
 
 @export var textures: Array[ObstacleSpriteData]
 
@@ -23,12 +24,14 @@ var speed: float = 200.0
 var speed_modifier: float = 1.0
 var stopped: bool = false
 
+
 func _ready() -> void:
+	visible_on_screen.screen_exited.connect(queue_free)
 	target_container.set_theme(PlayerConfig.get_theme())
 	set_textures()
 
 
-
+##Set up appropriate visual and collisions for randomly selected texture
 func set_textures() -> void:
 	chosen_texture = textures.pick_random()
 	sprite.texture = chosen_texture.texture
@@ -39,26 +42,24 @@ func set_textures() -> void:
 	collision_shape.position = chosen_texture.collidor_pos
 
 
+##Set and display target word on label
 func set_target_word(target: String) -> void:
 	target_word = target
 	word_label.set_text(target_word)
 
 
+##Move obstacle
 func _physics_process(_delta: float) -> void:
 	if not stopped:
 		velocity = direction * speed * speed_modifier
 		move_and_slide()
-	if position.x <= -10:
-		queue_free()
 
 
-func reset_word() -> void:
-	position.x = get_viewport_rect().size.x
-
-
+##Set visibility of the target word
 func hide_target(target_hidden: bool) -> void:
 	target_container.visible = !target_hidden
 
 
+##Speak word with TTS
 func speak_words() -> void:
 	PlayerConfig.speak_tts(target_word)
