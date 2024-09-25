1310a09514dcb9553e24e4069e266aafb3d705c2
Nishi790 <rebelnishi@gmail.com>
2024-09-24 23:17:37 -0400

    Story mode POC dialogue additions and stroke diagram addition

--- story_mode/UI/story_ui.gd
+++ story_mode/UI/story_ui.gd
@@ -6,7 +6,8 @@ signal input_received(text: String)
 @export var player_input: LineEdit
 @export var dialog_balloon: CanvasLayer
 @export var quest_display: QuestPanel
-
+@export var stroke_display: MarginContainer
+@export var stroke_label: RichTextLabel
 
 func _ready() -> void:
 	player_input.text_changed.connect(send_new_text)
@@ -34,6 +35,7 @@ func finish_quest(quest_name: String) -> void:
 
 
 func start_dialogue(key: String, dialogue: DialogueResource, nodes: Array[Node]) -> void:
+	nodes.append(self)
 	dialog_balloon.show()
 	dialog_balloon.start(dialogue, key, nodes)
 
@@ -41,3 +43,15 @@ func start_dialogue(key: String, dialogue: DialogueResource, nodes: Array[Node])
 func end_dialogue(_resource: DialogueResource) -> void:
 	dialog_balloon.hide()
 	player_input.grab_focus()
+
+
+##Shows a stroke in the center of the screen - for use as part of learning dialogues
+func display_stroke(text: String) -> void:
+	stroke_display.show()
+	stroke_label.clear()
+	stroke_label.push_paragraph(HORIZONTAL_ALIGNMENT_CENTER)
+	stroke_label.append_text(text)
+
+
+func hide_stroke() -> void:
+	stroke_display.hide()
