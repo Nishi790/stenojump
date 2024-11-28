1870212f31bd40e32bb17017898f352e33fcdc12
Nishi790 <rebelnishi@gmail.com>
2024-11-28 16:17:35 -0500

    Stronger typing

--- in_game/resource_templates/runner_level_data.gd
+++ in_game/resource_templates/runner_level_data.gd
@@ -2,7 +2,7 @@ class_name  RunnerLevel extends Resource
 
 @export var level_path: String
 @export var level_number: int
-@export var level_targets: Array
+@export var level_targets: Array[Dictionary]
 @export var level_order: LevelLoader.LevelOrder
 @export var default_level_size: int
 @export var next_level_path: String
